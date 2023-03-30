8a03868ca1645754a991df741b4040d15a0b5ef6
xmarcin3 <524836@mail.muni.cz>
2023-03-30 11:07:20 +0200

    .\buggy_python_code.py

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -25,6 +25,10 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
+    if (globals() not in (2, 3)) {
+        print(f"{globals()} is not a corrent urllib version")
+    }
+
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
  
